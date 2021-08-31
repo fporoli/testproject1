1675760010a1782f7e73103f7bd46ac50e115b00
fporoli <fporoli@gmail.com>
2021-08-31 18:38:58 +0200

    initial strapi-commit

---
+++ strapi-time-log-machine/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '370855056f23a490b7e553e9012eb13c'),
+    },
+  },
+});
